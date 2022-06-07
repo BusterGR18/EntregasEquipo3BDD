e561b2468f7f7d4573c7a8968c97a5e33f53d9c0
GusMa00 <99776520+GusMa00@users.noreply.github.com>
2022-06-06 23:53:24 -0500

    Añadir Recursos fragmentacion

---
+++ DAOFragmentacion/RecursosFrag/ConsultasFRAG.sql
@@ -0,0 +1,24 @@
+/*2. Listar datos del empleado que atendió más ordenes por territorio.
+3. Listar los datos del cliente con más ordenes solicitadas en la región “North America”.
+4. Listar el producto más solicitado en la región “Europe”.
+5. Listar las ofertas que tienen los productos de la categoría “Bikes”
+6. Listar los 3 productos menos solicitados en la región “Pacific”
+7. Actualizar la subcategoría de los productos con productId del 1 al 4 a la subcategoría valida para el tipo de producto.
+8. Listar los productos que no estén disponibles a la venta.
+9. Listar los clientes del territorio 1 y 4 que no tengan asociado un valor en personId
+10. Listar los clientes del territorio 1 que tengan ordenes en otro territorio*/
+
+
+--2 
+
+--3 
+select * from sales.SalesTerritory
+select * from Sales.SalesOrderHeader where (TerritoryID = 7 or TerritoryID = 8) 
+
+--4
+
+--5
+
+--6
+
+--7
