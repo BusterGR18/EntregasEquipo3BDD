3754ba28b5a4906577b1324f6d5284581ba7e251
GusMa00 <99776520+GusMa00@users.noreply.github.com>
2022-06-06 21:38:38 -0500

    Final CBASEPE.sql

--- Planes ejecucion/CbasePE.sql
+++ Planes ejecucion/CbasePE.sql
@@ -5,19 +5,13 @@ ALTER TABLE dbo.datoscovid alter column ID_REGISTRO nvarchar(15) not null
 ALTER TABLE dbo.datoscovid
 add constraint PK_datoscovid_ID_Registro PRIMARY KEY CLUSTERED(ID_REGISTRO)
 
---Indice Secundario:
+--Indices Secundario:
 CREATE INDEX IX_DatosCovid_FECHA_DEF on dbo.datoscovid (FECHA_DEF)
 CREATE INDEX IX_DatosCovid_CLASIFICACION_FINAL on dbo.datoscovid (CLASIFICACION_FINAL)
-
-
-DROP INDEX IX_DatosCovid_ENTIDAD_RES on dbo.datoscovid
-
 USE [covidHistorico]
-GO
 CREATE NONCLUSTERED INDEX [IX_DatosCovid_CLASIFICACION_FINAL_ENTIDAD]
-ON [dbo].[datoscovid] ([CLASIFICACION_FINAL])
+ON dbo.datoscovid (CLASIFICACION_FINAL)
 INCLUDE ([MUNICIPIO_RES],[ENTIDAD_RES])
-GO
 
 --Copia de la tabla sin llaves
 select *  into copiacovidh from [covidHistorico].[dbo].[datoscovid]
